82a0f2258fd61c75628aa78c9e793501b9c931b5
raonircl <raoni.afi@gmail.com>
2024-01-16 00:57:55 -0300

    ref: SSL commented

--- src/config/connection.js
+++ src/config/connection.js
@@ -8,7 +8,7 @@ const knex = require('knex')({
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
-        // ssl: { rejectUnauthorized: false }
+        ssl: { rejectUnauthorized: false }
     }
 });
 
